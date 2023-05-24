f5a2ddb92cfb78aadd8f2254c3c2247777ce2865
Cesar Santos <csar.santos18@gmail.com>
2023-05-24 09:18:52 -0500

    data structure in C

--- stackAndQueues.c
+++ stackAndQueues.c
@@ -1,2 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+typedef struct NO // Estrutura do No
+{
+    int data;
+    struct NO *next;
+} NO;
+
+typedef struct FILA // Estrutura da Fila
+{
+    NO *incio;
+    NO *fim;
+
+} FILA;
+
+FILA *criarFila()
+{
+    FILA *fila = (FILA *)malloc(sizeof(FILA));
+    fila->incio = NULL;
+    fila->fim = NULL;
+
+    return fila;
+}
+
+int isEmpty(FILA *fila)
+{
+    return (fila->incio == NULL);
+}
+
+void enqueue(FILA *fila, int data)
+{
+}
